596d5025505f0d92ebc6d0c1fc78278ea2964313
jpearson <jpearson556@gmail.com>
2011-08-18 09:33:21 -0600

    Static Pages passed testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ LifeControls::Application.routes.draw do
   get "pages/home"
   get "pages/tour"
   get "pages/about"
+  get "pages/faq"
+  get "pages/contact"
+  get "pages/testimonials"
+  get "pages/blog"
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
+    
     it "should be successful" do
       get 'home'
       response.should be_success
@@ -10,17 +12,92 @@ describe PagesController do
   end
   
   describe "GET 'tour'" do
-    it "should be successful" do
+    
+    before(:each) do
       get 'tour'
+    end
+    
+    it "should be successful" do
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Life-Controls | Tour")
     end
   end
   
   describe "GET 'about'" do
-    it "should be successful" do
+    
+    before(:each) do
       get 'about'
+    end
+    
+    it "should be successful" do
       response.should be_success
     end
+    
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Life-Controls | About Us")
+    end
   end
-
+  
+  describe "GET 'faq'" do
+    
+    before(:each) do
+      get 'faq'
+    end
+    
+    it "should be succesful" do
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Life-Controls | FAQ")
+    end
+  end
+  
+  describe "GET 'contact'" do
+    
+    before(:each) do
+      get 'contact'
+    end
+    
+    it "should be successful" do
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Life-Controls | Contact")
+    end
+  end
+  
+  describe "GET 'testimonials'" do
+    
+    before(:each) do
+      get 'testimonials'
+    end
+    
+    it "should be successful" do
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Life-Controls | Testimonials")
+    end
+  end
+  
+  describe "GET 'blog'" do
+    
+    before(:each) do
+      get 'blog'
+    end
+    
+    it "should be successful" do
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Life-Controls | Blog")
+    end
+  end
 end
